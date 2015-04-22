f10c16e3c87523fc6b1b2f47d6450b5ef06d4ce1
Heidi Williams-Foy <foywilliams@gmail.com>
2015-04-22 14:28:29 -0400

    trying to find out why grunt task not working

--- gruntfile.js
+++ gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
 		uglify: {
 			my_target: {
 				files: {
-				'grapepoint_wines/js/script.js': ['grapepoint_wines/components/js/*.js']
+				'grapepoint_wines/js/script.js' : ['grapepoint_wines/components/js/*.js']
 				}//files
 			} //my_target
 		}, //uglify
