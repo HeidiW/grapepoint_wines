027a2d522a171fc4589011ca4c39280d04a8cddf
Heidi Williams-Foy <foywilliams@gmail.com>
2015-04-21 20:06:58 -0400

    Gruntfiles in the process of being created, config.rb

---
+++ config.rb
@@ -0,0 +1,4 @@
+css_dir = 'grapepoint_wines/css'
+sass_dir = 'grapepoint_wines/components/sass'
+javascripts_dir = 'grapepoint_wines/js'
+output_style = :nested
--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,34 @@
 module.exports = function(grunt) {
-	grunt.loadNpm
-
-}
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.initConfig ({
+		uglify: {
+			my_target: {
+				files: {
+				'grapepoint_wines/js/script.js': ['grapepoint_wines/components/js/*.js']
+				}//files
+			} //my_target
+		}, //uglify
+		compass: {
+			dev: {
+				options: {
+					config: 'config.rb'
+				}//options
+			}//dev
+		}, //compass
+		watch: {
+			options: { livereload: true },
+			scripts: { }
+			files: ['grapepoint_wines/components/js/*.js'],
+			tasks: ['uglify']
+		} //script
+		sass: {
+			files: ['grapepoint_wines/components/sass/*.scss'],
+			tasks: ['compass:dev'] 
+		}, //sass
+		jade: {
+			files: ['*.jade'],
+		}
+	}) //initConfig
+	grunt.registerTask('default', 'watch');
+} //exports
